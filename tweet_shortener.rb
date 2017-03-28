d74d196602432606e29087a539f77d9c3f852620
Paisley Singh <paisley.singh@gmail.com>
2017-03-28 00:40:27 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,48 @@
-# Write your code here.
+def dictionary
+  dict = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter (tweet)
+  tweet = tweet.split
+  new_tweet = []
+  compare_words = dictionary
+  i = 0
+
+  while i < tweet.length
+    current_word = tweet[i]
+    current_word = compare_words[current_word.downcase] if compare_words.keys.include?(current_word.downcase)
+    new_tweet << current_word
+    i += 1
+  end
+
+  new_tweet.join(" ")
+end
+
+def bulk_tweet_shortener (tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener (tweet)
+  return word_substituter(tweet) if tweet.length >= 140
+  tweet
+end
+
+def shortened_tweet_truncator (tweet)
+  return word_substituter(tweet) if word_substituter(tweet).length <= 140
+
+  tweet = tweet[0..136] + "..."
+  tweet
+end
